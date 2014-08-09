b557f3850cbfa93663c658331a8c5158eeb60a87
Alex Clark <aclark@aclark.net>
2014-08-09 08:14:19 -0400

    Add file(s)

--- home_depot_crawl/spiders/homedepot.py
+++ home_depot_crawl/spiders/homedepot.py
@@ -2,8 +2,8 @@
 import scrapy
 
 
-class HomedepotcrawlSpider(scrapy.Spider):
-    name = "HomeDepotCrawl"
+class HomedepotSpider(scrapy.Spider):
+    name = "homedepot"
     allowed_domains = ["homedepot.com"]
     start_urls = (
         'http://www.homedepot.com/',
