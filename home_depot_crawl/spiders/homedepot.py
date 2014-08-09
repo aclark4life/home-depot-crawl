41f9b29e5b1eaca46ce735d85c16196937ca4e99
Alex Clark <aclark@aclark.net>
2014-08-09 09:45:51 -0400

    Update

--- home_depot_crawl/spiders/homedepot.py
+++ home_depot_crawl/spiders/homedepot.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from home_depot_crawl.items import LightBulb
 
 
-class HomedepotSpider(scrapy.Spider):
+class HomedepotSpider(Spider):
     name = "homedepot"
     allowed_domains = ["homedepot.com"]
     start_urls = (
@@ -10,4 +12,15 @@ class HomedepotSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        pass
+        """
+        """
+        sel = Selector(response)
+        bulbs = sel.xpath('//div[@id="products"]/div')
+        items = []
+
+        for bulb in bulbs:
+            item = LightBulb
+            #item['name'] = bulb.xpath('a/text()').extract()
+            items.append(item)
+
+        return items
